bb95fc7d0b9c7b25c4a4d709853eabf5cd792692
BTSimon <sbarthatoth@gmail.com>
2025-01-08 19:27:33 +0100

    Config

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require('path');
 const fs = require("fs");
 const app = express();
-const port = 3000;
+const port = 8080;
 const { v4: uuidv4 } = require('uuid');
 
 app.use(bodyParser.urlencoded({ extended: true }));
